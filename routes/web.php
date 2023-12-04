c427dc2e48f4f58960c2b57d1c8a9fd8b9d3ec1c
andre vale <andre_ramosdovale@outlook.com>
2023-12-04 14:40:24 -0300

    feat: create basic controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index']);
 
-Route::get('/about', function () {
-    return 'about';
-});
+Route::get('/about', [AboutController::class, 'index']);
 
-Route::get('/contact', function () {
-    return 'contact';
-});
+Route::get('/contact', [ContactController::class, 'index']);
