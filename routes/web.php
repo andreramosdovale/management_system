0ea007bc7a34bd2ba2bd449deb1a84edd4b8c2be
andre vale <andre_ramosdovale@outlook.com>
2023-12-04 14:38:35 -0300

    feat: create main controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/about', function () {
+    return 'about';
+});
+
+Route::get('/contact', function () {
+    return 'contact';
 });
